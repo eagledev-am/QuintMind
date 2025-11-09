3a195228d2112a348c657addf40f67006d9dc421
Abdelrahman Magdy <magdyabdo484@gmail.com>
2025-11-09 14:14:10 +0200

    Addning models for Users app

--- QuintMind/books/models.py
+++ QuintMind/books/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    Bio = models.TextField(blank=True, null=True)
+    Photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
+    DateOfBirth = models.DateField(blank=True, null=True)
+    Nationality = models.CharField(max_length=100, blank=True, null=True)
+    
+    role = models.CharField(max_length=50, default='user')
+
+    # Tell Django to use 'email' for login instead of 'username'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+
+class UserContact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
+    contact_type = models.CharField(max_length=50) # e.g., 'Website', 'Twitter'
+    contact_value = models.CharField(max_length=255) # e.g., 'https://myblog.com'
+
+    class Meta:
+        unique_together = ('user', 'contact_type') 
+
+    def __str__(self):
+        return f"{self.user.email} - {self.contact_type}"
+
+
+class UserFollowsAuthor(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_authors')
+   
+    author = models.ForeignKey('books.Author', on_delete=models.CASCADE, related_name='followers')
+    
+    class Meta:
+        unique_together = ('user', 'author') # Ensures a user can't follow the same author twice
+
+    def __str__(self):
+        return f"{self.user.email} follows {self.author.name}"
